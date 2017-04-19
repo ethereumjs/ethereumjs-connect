93c3e17f02f09d5152f943e9fdaf6bf6787a7a0c
Jack Peterson <jack@tinybike.net>
2017-04-18 20:01:02 -0700

    Updated set-network-id tests

--- test/set-network-id.js
+++ test/set-network-id.js
@@ -3,130 +3,64 @@
 "use strict";
 
 var assert = require("chai").assert;
-var clone = require("clone");
-var ethcon = require("../src");
+var setNetworkID = require("../src/set-network-id");
 
-describe("setNetworkID", function () {
+describe("set-network-id", function () {
   var test = function (t) {
-    var version = ethcon.rpc.version;
-    after(function () {
-      ethcon.rpc.version = version;
-    });
     describe(t.description, function () {
       it("sync", function () {
-        ethcon.rpc.version = function (callback) {
-          if (!callback) return t.blockchain.networkID;
-          callback(t.blockchain.networkID);
-        };
-        ethcon.state = clone(t.state);
+        var networkID;
         try {
-          ethcon.setNetworkID();
-          t.assertions(null, ethcon.state);
+          networkID = setNetworkID(t.rpc);
+          t.assertions(null, networkID);
         } catch (exc) {
-          t.assertions(exc, ethcon.state);
+          t.assertions(exc, networkID);
         }
-        ethcon.resetState();
       });
       it("async", function (done) {
-        ethcon.rpc.version = function (callback) {
-          if (!callback) return t.blockchain.networkID;
-          callback(t.blockchain.networkID);
-        };
-        ethcon.state = clone(t.state);
-        ethcon.setNetworkID(function (err) {
-          t.assertions(err, ethcon.state);
-          ethcon.resetState();
+        setNetworkID(t.rpc, function (err, networkID) {
+          t.assertions(err, networkID);
           done();
         });
       });
     });
   };
   test({
-    description: "network ID unchanged",
-    blockchain: {
-      networkID: "3"
+    description: "set current block number",
+    rpc: {
+      version: function (callback) {
+        if (!callback) return "3";
+        callback("3");
+      }
     },
-    state: {
-      from: "0xb0b",
-      coinbase: "0xb0b",
-      networkID: "3",
-      contracts: null,
-      allContracts: {
-        1: { myContract: "0xc1" },
-        3: { myContract: "0xc3" }
-      },
-      api: { events: null, functions: null },
-      connection: { http: "http://127.0.0.1:8545", ws: "ws://127.0.0.1:8546", ipc: null }
-    },
-    assertions: function (err, state) {
-      assert.isNull(err);
-      assert.strictEqual(state.networkID, "3");
+    assertions: function (err, networkID) {
+      assert.strictEqual(networkID, "3");
     }
   });
   test({
-    description: "change network ID from 1 to 3",
-    blockchain: {
-      networkID: "3"
+    description: "networkID is undefined",
+    rpc: {
+      version: function (callback) {
+        if (!callback) return undefined;
+        callback(undefined);
+      }
     },
-    state: {
-      from: "0xb0b",
-      coinbase: "0xb0b",
-      networkID: "1",
-      contracts: null,
-      allContracts: {
-        1: { myContract: "0xc1" },
-        3: { myContract: "0xc3" }
-      },
-      api: { events: null, functions: null },
-      connection: { http: "http://127.0.0.1:8545", ws: "ws://127.0.0.1:8546", ipc: null }
-    },
-    assertions: function (err, state) {
-      assert.isNull(err);
-      assert.strictEqual(state.networkID, "3");
+    assertions: function (err, networkID) {
+      assert.isTrue(err instanceof Error);
+      assert.isUndefined(networkID);
     }
   });
   test({
-    description: "rpc.version returns null",
-    blockchain: {
-      networkID: null
+    description: "networkID is null",
+    rpc: {
+      version: function (callback) {
+        if (!callback) return null;
+        callback(null);
+      }
     },
-    state: {
-      from: "0xb0b",
-      coinbase: "0xb0b",
-      networkID: "1",
-      contracts: null,
-      allContracts: {
-        1: { myContract: "0xc1" },
-        3: { myContract: "0xc3" }
-      },
-      api: { events: null, functions: null },
-      connection: { http: "http://127.0.0.1:8545", ws: "ws://127.0.0.1:8546", ipc: null }
-    },
-    assertions: function (err) {
-      assert.strictEqual(err.constructor, Error);
-      assert.strictEqual(err.message, "setNetworkID failed");
-    }
-  });
-  test({
-    description: "rpc.version returns an error",
-    blockchain: {
-      networkID: { error: "epic fail" }
-    },
-    state: {
-      from: "0xb0b",
-      coinbase: "0xb0b",
-      networkID: "1",
-      contracts: null,
-      allContracts: {
-        1: { myContract: "0xc1" },
-        3: { myContract: "0xc3" }
-      },
-      api: { events: null, functions: null },
-      connection: { http: "http://127.0.0.1:8545", ws: "ws://127.0.0.1:8546", ipc: null }
-    },
-    assertions: function (err) {
-      assert.strictEqual(err.constructor, Error);
-      assert.strictEqual(err.message, "epic fail");
+    assertions: function (err, networkID) {
+      assert.isTrue(err instanceof Error);
+      assert.isUndefined(networkID);
     }
   });
 });
