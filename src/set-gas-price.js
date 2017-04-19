f73ff9263303e0efcc46f2adec95799e5ec3443f
Jack Peterson <jack@tinybike.net>
2017-04-18 19:59:42 -0700

    Updated set-gas-price tests; fixed setGasPrice callback

--- src/set-gas-price.js
+++ src/set-gas-price.js
@@ -11,7 +11,7 @@ function setGasPrice(rpc, callback) {
   rpc.gasPrice(function (gasPrice) {
     if (!gasPrice) return callback(new Error("setGasPrice failed"));
     if (gasPrice.error) return callback(new Error(gasPrice.error));
-    callback(parseInt(gasPrice, 16));
+    callback(null, parseInt(gasPrice, 16));
   });
 }
 
--- test/set-gas-price.js
+++ test/set-gas-price.js
@@ -3,154 +3,64 @@
 "use strict";
 
 var assert = require("chai").assert;
-var clone = require("clone");
-var ethcon = require("../src");
+var setGasPrice = require("../src/set-gas-price");
 
-describe("setGasPrice", function () {
+describe("set-gas-price", function () {
   var test = function (t) {
-    var getGasPrice = ethcon.rpc.getGasPrice;
-    after(function () {
-      ethcon.rpc.getGasPrice = getGasPrice;
-    });
     describe(t.description, function () {
       it("sync", function () {
-        ethcon.state = clone(t.state);
-        ethcon.rpc.getGasPrice = function (callback) {
-          if (!callback) return t.blockchain.gasPrice;
-          callback(t.blockchain.gasPrice);
-        };
+        var gasPrice;
         try {
-          ethcon.setGasPrice();
-          t.assertions(null, ethcon.rpc, ethcon.state);
+          gasPrice = setGasPrice(t.rpc);
+          t.assertions(null, gasPrice);
         } catch (exc) {
-          t.assertions(exc, ethcon.rpc, ethcon.state);
+          t.assertions(exc, gasPrice);
         }
-        ethcon.resetState();
       });
       it("async", function (done) {
-        ethcon.state = clone(t.state);
-        ethcon.rpc.getGasPrice = function (callback) {
-          if (!callback) return t.blockchain.gasPrice;
-          callback(t.blockchain.gasPrice);
-        };
-        ethcon.setGasPrice(function (err) {
-          t.assertions(err, ethcon.rpc, ethcon.state);
-          ethcon.resetState();
+        setGasPrice(t.rpc, function (err, gasPrice) {
+          t.assertions(err, gasPrice);
           done();
         });
       });
     });
   };
   test({
-    description: "set rpc.gasPrice to latest block value",
-    blockchain: {
-      gasPrice: "0x4a817c801"
+    description: "set current gas price",
+    rpc: {
+      gasPrice: function (callback) {
+        if (!callback) return "0x1234";
+        callback("0x1234");
+      }
     },
-    state: {
-      from: "0xb0b",
-      coinbase: "0xb0b",
-      networkID: "3",
-      contracts: null,
-      allContracts: {
-        1: { myContract: "0xc1" },
-        3: { myContract: "0xc3" }
-      },
-      api: { events: null, functions: null },
-      connection: { http: "http://127.0.0.1:8545", ws: "ws://127.0.0.1:8546", ipc: null }
-    },
-    assertions: function (err, rpc, state) {
-      assert.isNull(err);
-      assert.strictEqual(rpc.gasPrice, 20000000001);
-      assert.deepEqual(state, {
-        from: "0xb0b",
-        coinbase: "0xb0b",
-        networkID: "3",
-        contracts: null,
-        allContracts: {
-          1: { myContract: "0xc1" },
-          3: { myContract: "0xc3" }
-        },
-        api: { events: null, functions: null },
-        connection: { http: "http://127.0.0.1:8545", ws: "ws://127.0.0.1:8546", ipc: null }
-      });
+    assertions: function (err, gasPrice) {
+      assert.strictEqual(gasPrice, 4660);
     }
   });
   test({
-    description: "rpc.gasPrice the same as latest block value",
-    blockchain: {
-      gasPrice: "0x4a817c800"
+    description: "gasPrice is undefined",
+    rpc: {
+      gasPrice: function (callback) {
+        if (!callback) return undefined;
+        callback(undefined);
+      }
     },
-    state: {
-      from: "0xb0b",
-      coinbase: "0xb0b",
-      networkID: "3",
-      contracts: null,
-      allContracts: {
-        1: { myContract: "0xc1" },
-        3: { myContract: "0xc3" }
-      },
-      api: { events: null, functions: null },
-      connection: { http: "http://127.0.0.1:8545", ws: "ws://127.0.0.1:8546", ipc: null }
-    },
-    assertions: function (err, rpc, state) {
-      assert.isNull(err);
-      assert.strictEqual(rpc.gasPrice, 20000000000);
-      assert.deepEqual(state, {
-        from: "0xb0b",
-        coinbase: "0xb0b",
-        networkID: "3",
-        contracts: null,
-        allContracts: {
-          1: { myContract: "0xc1" },
-          3: { myContract: "0xc3" }
-        },
-        api: { events: null, functions: null },
-        connection: { http: "http://127.0.0.1:8545", ws: "ws://127.0.0.1:8546", ipc: null }
-      });
+    assertions: function (err, gasPrice) {
+      assert.isTrue(err instanceof Error);
+      assert.isUndefined(gasPrice);
     }
   });
   test({
-    description: "rpc.getGasPrice returns null",
-    blockchain: {
-      gasPrice: null
+    description: "gasPrice is null",
+    rpc: {
+      gasPrice: function (callback) {
+        if (!callback) return null;
+        callback(null);
+      }
     },
-    state: {
-      from: "0xb0b",
-      coinbase: "0xb0b",
-      networkID: "3",
-      contracts: null,
-      allContracts: {
-        1: { myContract: "0xc1" },
-        3: { myContract: "0xc3" }
-      },
-      api: { events: null, functions: null },
-      connection: { http: "http://127.0.0.1:8545", ws: "ws://127.0.0.1:8546", ipc: null }
-    },
-    assertions: function (err) {
-      assert.strictEqual(err.constructor, Error);
-      assert.strictEqual(err.message, "setGasPrice failed");
-    }
-  });
-  test({
-    description: "rpc.getGasPrice returns an error",
-    blockchain: {
-      gasPrice: { error: "epic fail" }
-    },
-    state: {
-      from: "0xb0b",
-      coinbase: "0xb0b",
-      networkID: "3",
-      contracts: null,
-      allContracts: {
-        1: { myContract: "0xc1" },
-        3: { myContract: "0xc3" }
-      },
-      api: { events: null, functions: null },
-      connection: { http: "http://127.0.0.1:8545", ws: "ws://127.0.0.1:8546", ipc: null }
-    },
-    assertions: function (err) {
-      assert.strictEqual(err.constructor, Error);
-      assert.strictEqual(err.message, "epic fail");
+    assertions: function (err, gasPrice) {
+      assert.isTrue(err instanceof Error);
+      assert.isUndefined(gasPrice);
     }
   });
 });
