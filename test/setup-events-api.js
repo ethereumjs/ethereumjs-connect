93c1472af84fc5d60789242d5a23ed72c279375f
Jack Peterson <jack@tinybike.net>
2017-04-18 20:05:39 -0700

    Updated setup-events-api tests

--- test/setup-events-api.js
+++ test/setup-events-api.js
@@ -3,126 +3,53 @@
 "use strict";
 
 var assert = require("chai").assert;
-var clone = require("clone");
-var ethcon = require("../src");
+var setupEventsAPI = require("../src/setup-events-api")
 
-describe("setupEventsAPI", function () {
+describe("setup-events-api", function () {
   var test = function (t) {
     it(t.description, function () {
-      ethcon.state = clone(t.state);
-      ethcon.setupEventsAPI();
-      t.assertions(ethcon.state);
-      ethcon.resetState();
+      t.assertions(setupEventsAPI(t.params.eventsAPI, t.params.contracts));
     });
   };
   test({
-    description: "set up events API, do not modify null functions API",
-    state: {
-      from: "0xb0b",
-      coinbase: "0xb0b",
-      networkID: "3",
+    description: "set up events API",
+    params: {
+      eventsAPI: {
+        event1: { contract: "contract1" },
+        event2: { contract: "contract1" },
+        event3: { contract: "contract2" }
+      },
       contracts: {
         contract1: "0xc1",
         contract2: "0xc2"
       },
-      allContracts: {
-        3: {
-          contract1: "0xc1",
-          contract2: "0xc2"
-        }
-      },
-      api: {
-        events: {
-          event1: { contract: "contract1" },
-          event2: { contract: "contract1" },
-          event3: { contract: "contract2" }
-        },
-        functions: null
-      },
-      connection: { http: "http://127.0.0.1:8545", ws: "ws://127.0.0.1:8546", ipc: null }
     },
-    assertions: function (state) {
-      assert.deepEqual(state.api.events, {
+    assertions: function (eventsAPI) {
+      assert.deepEqual(eventsAPI, {
         event1: { address: "0xc1", contract: "contract1" },
         event2: { address: "0xc1", contract: "contract1" },
         event3: { address: "0xc2", contract: "contract2" }
       });
-      assert.isNull(state.api.functions);
     }
   });
   test({
-    description: "modify existing events API, do not modify null functions API",
-    state: {
-      from: "0xb0b",
-      coinbase: "0xb0b",
-      networkID: "3",
+    description: "modify existing events API",
+    params: {
+      eventsAPI: {
+        event1: { address: "0xC1", contract: "contract1" },
+        event2: { address: "0xC1", contract: "contract1" },
+        event3: { address: "0xC2", contract: "contract2" }
+      },
       contracts: {
         contract1: "0xc1",
         contract2: "0xc2"
-      },
-      allContracts: {
-        3: {
-          contract1: "0xc1",
-          contract2: "0xc2"
-        }
-      },
-      api: {
-        events: {
-          event1: { address: "0xC1", contract: "contract1" },
-          event2: { address: "0xC1", contract: "contract1" },
-          event3: { address: "0xC2", contract: "contract2" }
-        },
-        functions: null
-      },
-      connection: { http: "http://127.0.0.1:8545", ws: "ws://127.0.0.1:8546", ipc: null }
+      }
     },
-    assertions: function (state) {
-      assert.deepEqual(state.api.events, {
+    assertions: function (eventsAPI) {
+      assert.deepEqual(eventsAPI, {
         event1: { address: "0xc1", contract: "contract1" },
         event2: { address: "0xc1", contract: "contract1" },
         event3: { address: "0xc2", contract: "contract2" }
-      });
-      assert.isNull(state.api.functions);
-    }
-  });
-  test({
-    description: "modify existing events API, do not modify existing functions API",
-    state: {
-      from: "0xb0b",
-      coinbase: "0xb0b",
-      networkID: "3",
-      contracts: {
-        contract1: "0xc1",
-        contract2: "0xc2"
-      },
-      allContracts: {
-        3: {
-          contract1: "0xc1",
-          contract2: "0xc2"
-        }
-      },
-      api: {
-        events: {
-          event1: { address: "0xC1", contract: "contract1" },
-          event2: { address: "0xC1", contract: "contract1" },
-          event3: { address: "0xC2", contract: "contract2" }
-        },
-        functions: {
-          contract1: { method1: { to: "0xC1" }, method2: { to: "0xC1" } },
-          contract2: { method1: { to: "0xC2" } }
-        }
-      },
-      connection: { http: "http://127.0.0.1:8545", ws: "ws://127.0.0.1:8546", ipc: null }
-    },
-    assertions: function (state) {
-      assert.deepEqual(state.api.events, {
-        event1: { address: "0xc1", contract: "contract1" },
-        event2: { address: "0xc1", contract: "contract1" },
-        event3: { address: "0xc2", contract: "contract2" }
-      });
-      assert.deepEqual(state.api.functions, {
-        contract1: { method1: { to: "0xC1" }, method2: { to: "0xC1" } },
-        contract2: { method1: { to: "0xC2" } }
       });
     }
   });
