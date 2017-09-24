e2f2d7b97b924f29b604fc0a04094f4cc4bde61d
Jack Peterson <jack@tinybike.net>
2017-09-24 00:15:10 -0700

    ethrpc@4.3.0

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ var setupFunctionsABI = require("./setup-functions-abi");
 var connect = require("./connect");
 
 module.exports = {
-  version: "4.3.0",
+  version: "4.3.2",
   setFrom: setFrom,
   setupEventsABI: setupEventsABI,
   setupFunctionsABI: setupFunctionsABI,
